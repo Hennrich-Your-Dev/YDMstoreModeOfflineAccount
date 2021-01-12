955ea3ddfdddea775f07a60ae812c46678d5ec74
Douglas Hennrich <douglashennrich@yourdev.com.br>
2021-01-12 13:26:46 -0300

    First commit

---
+++ Pods/Target Support Files/Pods-YDMstoreModeOfflineAccount/Pods-YDMstoreModeOfflineAccount-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_YDMstoreModeOfflineAccount : NSObject
+@end
+@implementation PodsDummy_Pods_YDMstoreModeOfflineAccount
+@end
---
+++ YDMstoreModeOfflineAccount/YDMstoreModeOfflineAccount.h
@@ -0,0 +1,18 @@
+//
+//  YDMstoreModeOfflineAccount.h
+//  YDMstoreModeOfflineAccount
+//
+//  Created by Douglas on 12/01/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for YDMstoreModeOfflineAccount.
+FOUNDATION_EXPORT double YDMstoreModeOfflineAccountVersionNumber;
+
+//! Project version string for YDMstoreModeOfflineAccount.
+FOUNDATION_EXPORT const unsigned char YDMstoreModeOfflineAccountVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YDMstoreModeOfflineAccount/PublicHeader.h>
+
+
